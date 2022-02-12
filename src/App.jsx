3011921a6210e304a77c4469c568c56a1701404a
yasirnurprasetya <darkinvander1892@gmail.com>
2022-02-12 17:16:14 +0700

    Add sidebar menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Portofolio from "./components/portofolio/Portofolio";
 import Works from "./components/works/Works";
 import Testimonial from "./components/testimonial/Testimonial";
 import Contact from "./components/contact/Contact";
+import Menu from "./components/menu/Menu"
 import "./app.scss";
 import {useState} from "react"
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro />
         <Portofolio />
